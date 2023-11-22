aca39e50415b1f31dab3b52047f0c33a3199cc7d
H-unter <99230734+H-unter@users.noreply.github.com>
2023-11-22 14:22:02 +1000

    Add greeting app route

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return '<h1>Hello World!!</h1>'
+    return '<h1>Hello World :)</h1>'
 
+@app.route('/greet')
+@app.route('/greet/<name>')
+def greet(name=""):
+    return f"Hello {name}"
 
 if __name__ == '__main__':
     app.run()

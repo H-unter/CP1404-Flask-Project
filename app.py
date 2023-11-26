bb44f58ef15734fc9720bdea7bfc42f437d06063
H-unter <99230734+H-unter@users.noreply.github.com>
2023-11-26 16:25:29 +1000

    Add docstring

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+""""
+CP1404 Practical 10 Hunter Kruger-Ilingworth
+flaskproject
+"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,15 +11,18 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return '<h1>Hello World :)</h1>'
 
+
 @app.route('/greet')
 @app.route('/greet/<name>')
 def greet(name=""):
     return f"Hello {name}"
 
+
 @app.route('/c')
 @app.route('/c/<input>')
 def convert(input=0.0):
-    return f"{convert_celsius_to_fahrenheit(float(input))}"
+    return f"<h1>{input}°C is equal to {convert_celsius_to_fahrenheit(float(input))}°F</h1>"
+
 
 def convert_celsius_to_fahrenheit(celsius):
     """Convert Celsius to Fahrenheit"""
